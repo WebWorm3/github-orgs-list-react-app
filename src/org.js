1146cae883138a7cce6b36815808165aa945a60d
WebWorm3 <Ivan2002dav@gmail.com>
2018-04-13 21:32:19 +0300

    Fixed org.js

--- src/org.js
+++ src/org.js
@@ -2,35 +2,17 @@ import React, { Component } from 'react';
 // import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 let classes = 'org-avatar ';
 
-class Org extends Component{
-  state = {
-    isLoaded: false
+  class Org extends Component {
+
+    render(){
+      var copy = Object.assign({}, this.props.selectedOrg);
+      return(
+        <div>
+          <h1>{copy.login}</h1>
+          <img className={classes} src={copy.avatar_url}></img>
+        </div>
+      );
+    }
   }
-
-  handleLoaded(){
-
-  }
-
-  handleChange(){
-
-  }
-
-  render(){
-    return(
-      <ThisOrg handleLoaded={this.handleLoaded.bind(this)} handleChange={this.handleChange.bind(this)} selectedOrg={this.props.selectedOrg} />
-    );
-  }
-}
-
-const ThisOrg = ({ selectedOrg, handleLoaded, handleChange}) => {
-  var copy = Object.assign({}, selectedOrg);
-
-  return (
-    <div>
-      <h1>{copy.login}</h1>
-      <img className={classes} src={copy.avatar_url}></img>
-    </div>
-  );
-}
 
 export default Org;
