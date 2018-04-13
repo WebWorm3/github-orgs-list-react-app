84b484efe5aa986b8a06af413e905fa42539cfae
WebWorm3 <Ivan2002dav@gmail.com>
2018-04-13 20:23:15 +0300

    Fixed some bugs

--- src/org.js
+++ src/org.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 // import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-let classes = 'org-avatar avatar-none';
+let classes = 'org-avatar ';
+
 class Org extends Component{
   state = {
     isLoaded: false
   }
 
   handleLoaded(){
-    classes = 'org-avatar avatar-none';
+
   }
 
   handleChange(){
-    setInterval(this.setState({isLoaded: true}), 2000);
+
   }
 
   render(){
     return(
-      <ThisOrg loadfunc={this.handleLoaded.bind(this)} changefunc={this.handleChange.bind(this)} selectedOrg={this.props.selectedOrg} />
+      <ThisOrg handleLoaded={this.handleLoaded.bind(this)} handleChange={this.handleChange.bind(this)} selectedOrg={this.props.selectedOrg} />
     );
   }
 }
 
-const ThisOrg = ({ selectedOrg, loadfunc, changefunc}) => {
+const ThisOrg = ({ selectedOrg, handleLoaded, handleChange}) => {
   var copy = Object.assign({}, selectedOrg);
 
   return (
-    <div onLoad={changefunc}>
+    <div>
       <h1>{copy.login}</h1>
       <img className={classes} src={copy.avatar_url}></img>
     </div>
